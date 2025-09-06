a38ded7731fffd4ea05386eb592af429831a4dc2
tomascabrerizo <tomascabrerizo97@gmail.com>
2025-09-06 18:50:54 -0300

    simple abstraction layer on top of win32 sockets

--- src/net.c
+++ src/net.c
@@ -6,131 +6,65 @@
 
 #define TCP_CONN_DEFAULT_BUFFER_SIZE (kb(10))
 
+typedef struct ConnAddr {
+  struct sockaddr_in addr_in;
+} ConnAddr;
+
 typedef struct ConnSet {
   fd_set fds;
-  struct ConnSet *next;
 } ConnSet;
 
-typedef struct ConnTimeout {
-  struct timeval *val_ptr;
-  struct timeval val;
-  struct ConnTimeout *next;
-} ConnTimeout;
-
-typedef struct ConnTCP {
-  SOCKET sock;
-  struct sockaddr_in addr;
-  u8 recv_buffer[TCP_CONN_DEFAULT_BUFFER_SIZE];
-  u64 recv_buffer_used;
-  b32 farming;
-  u32 bytes_to_farm;
-  struct ConnTCP *next;
-} ConnTCP;
-
-typedef struct ConnContext {
-  Arena *arena;
-  ConnTCP *tcp_first_free;
-  ConnSet *set_first_free;
-  ConnTimeout *timeout_first_free;
-} ConnContext;
-
-static ConnContext context;
-
-ConnTCP *conn_tcp_alloc(void) {
-  ConnTCP *conn;
-  if (context.tcp_first_free) {
-    conn = context.tcp_first_free;
-    context.tcp_first_free = context.tcp_first_free->next;
-  } else {
-    conn = arena_alloc(context.arena, sizeof(*conn), 8);
-  }
-  assert(conn);
-  memset(conn, 0, sizeof(*conn));
-  conn->sock = INVALID_SOCKET;
-  return conn;
-}
-
-void conn_tcp_free(ConnTCP *conn) {
-  assert(conn);
-  conn->next = context.tcp_first_free;
-  context.tcp_first_free = conn;
-}
-
-void conn_init(Arena *arena) {
+void conn_init(void) {
   WSADATA wsa_data;
   WSAStartup(MAKEWORD(2, 2), &wsa_data);
-  memset(&context, 0, sizeof(context));
-  context.arena = arena;
 }
 
-ConnAddress conn_address(Arena *arena, char *address, u16 port) {
-  struct sockaddr_in *addr;
-  addr = arena_alloc(arena, sizeof(struct sockaddr_in), 8);
-  addr->sin_family = AF_INET;
-  addr->sin_port = htons(port);
-  inet_pton(addr->sin_family, address, &addr->sin_addr);
-  return (ConnAddress)addr;
+ConnAddr *conn_address_create(Arena *arena) {
+  ConnAddr *addr;
+  addr = arena_alloc(arena, sizeof(*addr), 8);
+  memset(addr, 0, sizeof(*addr));
+  return addr;
 }
 
-ConnSet *conn_set_create(void) {
+ConnAddr *conn_address(Arena *arena, char *address, u16 port) {
+  ConnAddr *addr = conn_address_create(arena);
+  addr->addr_in.sin_family = AF_INET;
+  addr->addr_in.sin_port = htons(port);
+  inet_pton(addr->addr_in.sin_family, address, &addr->addr_in.sin_addr);
+  return addr;
+}
+
+ConnSet *conn_set_create(Arena *arena) {
   ConnSet *set;
-  if (context.set_first_free) {
-    set = context.set_first_free;
-    context.set_first_free = context.set_first_free->next;
-  } else {
-    set = arena_alloc(context.arena, sizeof(*set), 8);
-  }
+  set = arena_alloc(arena, sizeof(*set), 8);
   assert(set);
   memset(set, 0, sizeof(*set));
   return set;
 }
 
-void conn_set_destroy(ConnSet *set) {
-  assert(set);
-  set->next = context.set_first_free;
-  context.set_first_free = set;
-}
-
 void conn_set_clear(ConnSet *set) { FD_ZERO(&set->fds); }
 
-void conn_set_add(ConnSet *set, struct ConnTCP *conn) {
-  FD_SET(conn->sock, &set->fds);
+void conn_set_add(ConnSet *set, Conn conn) {
+  SOCKET sock;
+  sock = (SOCKET)conn;
+  FD_SET(sock, &set->fds);
 }
 
-b32 conn_set_has(ConnSet *set, ConnTCP *conn) {
-  return FD_ISSET(conn->sock, &set->fds);
+b32 conn_set_has(ConnSet *set, Conn conn) {
+  SOCKET sock;
+  sock = (SOCKET)conn;
+  return FD_ISSET(sock, &set->fds);
 }
 
-ConnTimeout *conn_timeout_create(void) {
-  ConnTimeout *timeout;
-  if (context.timeout_first_free) {
-    timeout = context.timeout_first_free;
-    context.timeout_first_free = context.timeout_first_free->next;
-  } else {
-    timeout = arena_alloc(context.arena, sizeof(*timeout), 8);
-  }
-  assert(timeout);
-  memset(timeout, 0, sizeof(*timeout));
-  return timeout;
-}
-
-void conn_timeout_destroy(ConnTimeout *timeout) {
-  assert(timeout);
-  timeout->next = context.timeout_first_free;
-  context.timeout_first_free = timeout;
-}
-
-void conn_timeout_set(struct ConnTimeout *timeout, s32 ms) {
-  timeout->val_ptr = 0;
-  if (ms > -1) {
-    timeout->val.tv_sec = ms / 1000;
-    timeout->val.tv_usec = (ms % 1000) * 1000;
-    timeout->val_ptr = &timeout->val;
-  }
-}
-
-s32 conn_select(ConnSet *read, ConnSet *write, ConnTimeout *timeout) {
+s32 conn_select(ConnSet *read, ConnSet *write, s32 ms) {
+  struct timeval val, *val_ptr;
   fd_set *fd_read, *fd_write;
+  val_ptr = 0;
+  if (ms > -1) {
+    val.tv_sec = ms / 1000;
+    val.tv_usec = (ms % 1000) * 1000;
+    val_ptr = &val;
+  }
   fd_read = fd_write = 0;
   if (read) {
     fd_read = &read->fds;
@@ -138,68 +72,104 @@ s32 conn_select(ConnSet *read, ConnSet *write, ConnTimeout *timeout) {
   if (write) {
     fd_write = &write->fds;
   }
-  return select(0, fd_read, fd_write, 0, timeout->val_ptr);
+  return select(0, fd_read, fd_write, 0, val_ptr);
 }
 
-ConnTCP *conn_tcp_create(void) {
-  ConnTCP *conn;
+Conn conn_tcp(void) {
   SOCKET sock;
-
   sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-  if (sock == INVALID_SOCKET) {
-    return 0;
-  }
-  conn = conn_tcp_alloc();
-  conn->sock = sock;
-  return conn;
+  return (Conn)sock;
 }
 
-void conn_tcp_destroy(ConnTCP *conn) {
-  assert(conn);
-  closesocket(conn->sock);
-  conn_tcp_free(conn);
-}
-
-b32 conn_tcp_listen(ConnTCP *conn, u16 port) {
-  struct sockaddr_in addr;
-  assert(conn->sock != INVALID_SOCKET);
-
-  addr.sin_family = AF_INET;
-  addr.sin_addr.s_addr = INADDR_ANY;
-  addr.sin_port = htons(port);
-  if (bind(conn->sock, (struct sockaddr *)&addr, sizeof(addr)) ==
-      SOCKET_ERROR) {
-    return false;
-  }
-  if (listen(conn->sock, SOMAXCONN) == SOCKET_ERROR) {
-    return false;
-  }
-
-  conn->addr = addr;
-  return true;
-}
-
-b32 conn_tcp_connect(ConnTCP *conn, ConnAddress address) {
-  assert(conn->sock != INVALID_SOCKET);
-  if (connect(conn->sock, (struct sockaddr *)address,
-              (int)sizeof(struct sockaddr_in)) == SOCKET_ERROR) {
-    return false;
-  }
-  return true;
-}
-
-struct ConnTCP *conn_tcp_accept(struct ConnTCP *conn) {
+Conn conn_udp(void) {
   SOCKET sock;
-  struct sockaddr_in addr;
-  s32 addr_len;
-  ConnTCP *other;
-  addr_len = sizeof(addr);
-  sock = accept(conn->sock, (struct sockaddr *)&addr, &addr_len);
-  if (sock == INVALID_SOCKET) {
-    return 0;
+  sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+  return (Conn)sock;
+}
+
+b32 conn_bind(Conn conn, ConnAddr *addr) {
+  SOCKET sock;
+  sock = (SOCKET)conn;
+  if (bind(sock, (struct sockaddr *)&addr->addr_in,
+           (int)sizeof(addr->addr_in)) == SOCKET_ERROR) {
+    return false;
   }
-  other = conn_tcp_alloc();
-  other->sock = sock;
-  other->addr = addr;
-  return other;
+  return true;
+}
+
+b32 conn_listen(Conn conn) {
+  SOCKET sock;
+  sock = (SOCKET)conn;
+  if (listen(sock, SOMAXCONN) == SOCKET_ERROR) {
+    return false;
+  }
+  return true;
+}
+
+b32 conn_connect(Conn conn, ConnAddr *addr) {
+  SOCKET sock;
+  sock = (SOCKET)conn;
+  assert(sock != INVALID_SOCKET);
+  if (connect(sock, (struct sockaddr *)&addr->addr_in,
+              (int)sizeof(addr->addr_in)) == SOCKET_ERROR) {
+    return false;
+  }
+  return true;
+}
+
+Conn conn_accept(Conn conn, ConnAddr *addr) {
+  SOCKET sock, other;
+  s32 other_addr_len;
+  struct sockaddr_in other_addr;
+  other_addr_len = sizeof(other_addr);
+  sock = (SOCKET)conn;
+  other = accept(sock, (struct sockaddr *)&other_addr, &other_addr_len);
+  if (addr) {
+    addr->addr_in = other_addr;
+  }
+  return (Conn)other;
+}
+
+s32 conn_read_from(Conn conn, u8 *buffer, u32 size, ConnAddr *from) {
+  s32 res, addr_size;
+  SOCKET sock;
+  sock = (SOCKET)conn;
+  addr_size = (u32)sizeof(from->addr_in);
+  res = recvfrom(sock, (char *)buffer, size, 0,
+                 (struct sockaddr *)&from->addr_in, &addr_size);
+  return res;
+}
+
+s32 conn_write_to(Conn conn, u8 *buffer, u32 size, ConnAddr *to) {
+  s32 res, addr_size;
+  SOCKET sock;
+  sock = (SOCKET)conn;
+  addr_size = (u32)sizeof(to->addr_in);
+  res = sendto(sock, (char *)buffer, size, 0, (struct sockaddr *)&to->addr_in,
+               addr_size);
+  return res;
+}
+
+s32 conn_read(Conn conn, u8 *buffer, u32 size) {
+  SOCKET sock;
+  sock = (SOCKET)conn;
+  return recv(sock, (char *)buffer, size, 0);
+}
+
+s32 conn_write(Conn conn, u8 *buffer, u32 size) {
+  SOCKET sock;
+  sock = (SOCKET)conn;
+  return send(sock, (char *)buffer, size, 0);
+}
+
+void conn_close(Conn conn) {
+  SOCKET sock;
+  sock = (SOCKET)conn;
+  closesocket(sock);
+}
+
+b32 conn_valid(Conn conn) {
+  SOCKET sock;
+  sock = (SOCKET)conn;
+  return sock != INVALID_SOCKET;
 }
--- src/net.h
+++ src/net.h
@@ -1,33 +1,39 @@
 #ifndef _NET_H_
 #define _NET_H_
 
-struct Arena;
-void conn_init(struct Arena *arena);
+#include "core.h"
 
-typedef void *ConnAddress;
+typedef u64 Conn;
+struct ConnAddr;
 struct ConnSet;
-struct ConnTimeout;
-struct ConnTCP;
+struct Arena;
 
-ConnAddress conn_address(struct Arena *arena, char *address, u16 port);
+void conn_init(void);
 
-struct ConnSet *conn_set_create(void);
-void conn_set_destroy(struct ConnSet *set);
+struct ConnAddr *conn_address_create(struct Arena *arena);
+struct ConnAddr *conn_address(struct Arena *arena, char *address, u16 port);
+
+struct ConnSet *conn_set_create(struct Arena *arena);
 void conn_set_clear(struct ConnSet *set);
-void conn_set_add(struct ConnSet *set, struct ConnTCP *conn);
-b32 conn_set_has(struct ConnSet *set, struct ConnTCP *conn);
+void conn_set_add(struct ConnSet *set, Conn conn);
+b32 conn_set_has(struct ConnSet *set, Conn conn);
 
-struct ConnTimeout *conn_timeout_create(void);
-void conn_timeout_destroy(struct ConnTimeout *timeout);
-void conn_timeout_set(struct ConnTimeout *timeout, s32 ms);
+s32 conn_select(struct ConnSet *read, struct ConnSet *write, s32 ms);
 
-s32 conn_select(struct ConnSet *read, struct ConnSet *write,
-                struct ConnTimeout *timeout);
+Conn conn_tcp(void);
+Conn conn_udp(void);
 
-struct ConnTCP *conn_tcp_create(void);
-void conn_tcp_destroy(struct ConnTCP *conn);
-b32 conn_tcp_listen(struct ConnTCP *conn, u16 port);
-b32 conn_tcp_connect(struct ConnTCP *conn, ConnAddress address);
-struct ConnTCP *conn_tcp_accept(struct ConnTCP *conn);
+b32 conn_bind(Conn conn, struct ConnAddr *addr);
+b32 conn_listen(Conn conn);
+b32 conn_connect(Conn conn, struct ConnAddr *addr);
+Conn conn_accept(Conn conn, struct ConnAddr *addr);
+
+s32 conn_read(Conn conn, u8 *buffer, u32 size);
+s32 conn_write(Conn conn, u8 *buffer, u32 size);
+s32 conn_read_from(Conn conn, u8 *buffer, u32 size, struct ConnAddr *from);
+s32 conn_write_to(Conn conn, u8 *buffer, u32 size, struct ConnAddr *to);
+
+void conn_close(Conn conn);
+b32 conn_valid(Conn conn);
 
 #endif
